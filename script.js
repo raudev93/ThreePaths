2dbd0145e95c846aeadcd34dcb643b6b4b3fa946
UzielAR <uziel.anguiano@gmail.com>
2022-05-12 22:56:17 -0500

    Console Game Completed

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+const paths = ["rock","paper","scissor"]
 function computerPlay(){
-    return Math.round(Math.random() * 3)
+    const randomChoice = Math.floor(Math.random() * 3)
+    return paths[randomChoice]
 }
 function getPlayerSelection(){
     return formatSelection(prompt("Choise your Path"))
@@ -9,15 +11,42 @@ function formatSelection(selection){
 }
 
 function playRound(playerSelection,computerSelection){
-    
+    if(playerSelection === computerSelection){
+        return "It's a Tie"
+    }
+    if(playerSelection === "rock"){
+        if(computerSelection === "paper"){
+            return "You Lose! Paper beats Rock"
+        }
+        else{
+            return "You Win! Rock beats Scissor"
+        }
+    }
+    else if(playerSelection === "paper"){
+        if(computerSelection === "scissor"){
+            return "You Lose! Scissor beats Paper"
+        }
+        else{
+            return "You Win! Paper beats Rock"
+        }
+    }
+    else{
+        if(computerSelection === "rock"){
+            return "You Lose! Rock beats Scissor"
+        }
+        else{
+            return "You Win! Scissor beats Paper"
+        }
+    }
 }
 
 function game(){
     for(let i = 0; i< 5; i++){
         console.log(`Round: ${i+1}`)
-        // const playerSelection = getPlayerSelection()
-        // const computerSelection = computerPlay()
-        // const result = playRound(playerSelection,computerSelection)
+        const playerSelection = getPlayerSelection()
+        const computerSelection = computerPlay()
+        const result = playRound(playerSelection,computerSelection)
+        console.log(result)
     }
 }
 game()
